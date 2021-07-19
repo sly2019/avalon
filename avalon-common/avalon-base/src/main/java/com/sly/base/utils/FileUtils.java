08bb186883aefc49ea26797c533b05961abace58
sly-zjlz <mrzj_sh@163,com>
2021-07-19 09:46:21 +0800

    add common base and core

---
+++ avalon-common/avalon-base/src/main/java/com/sly/base/utils/FileUtils.java
@@ -0,0 +1,60 @@
+package com.sly.base.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @Author by wj on 2021/7/9 15:57
+ * @Description TODO
+ */
+@Slf4j
+public class FileUtils {
+
+    /**
+     * 递归获取指定层级的文件或目录
+     * @param dir
+     * @param level
+     * @param fileList
+     */
+    public static void getSpecifiedLevel(File dir, int level, List<File> fileList){
+        if(level < 1){
+            throw new RuntimeException("level must be greater than or equal to 1");
+        }
+        if(dir == null){
+            return;
+        }
+        File[] files = dir.listFiles();
+        if(files == null){
+            return;
+        }
+        for(int i = 0; i < files.length; i++){
+            if(level == 1){
+                fileList.add(files[i]);
+            } else {
+                File file = files[i];
+                if(file.isDirectory()){
+                    getSpecifiedLevel(file, level - 1, fileList);
+                }
+            }
+        }
+    }
+
+    /**
+     * 递归删除文件
+     * @param file
+     */
+    public static void delFile(File file){
+        if(file.isDirectory()){
+            File[] files = file.listFiles();
+            for(File file1 : files){
+                delFile(file1);
+            }
+            file.delete();
+        } else {
+            file.delete();
+        }
+    }
+
+}
--- avalon-common/avalon-common-base/src/main/java/com/sly/common/Test.java
+++
@@ -1,8 +0,0 @@
-package com.sly.common;
-
-/**
- * @Author by wj on 2021/7/8 19:31
- * @Description TODO
- */
-public class Test {
-}
